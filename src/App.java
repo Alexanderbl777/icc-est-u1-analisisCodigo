e4f881cf3dd39c1d93becd18adc01e28bce3e9dd
Alexander <miguebeltranbl@gmail.com>
2025-04-29 19:55:50 -0500

    Actividad

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) {
 
+        
+        BenchMarking bM = new BenchMarking();
+
+        /** 
         MetodosOrdenamiento ordenador = new MetodosOrdenamiento();
         int[] arregloBurbuja = { 34, 2, 10, 6, 7, 5, 1, 15 };
         int[] arregloSeleccion = { 34, 2, 10, 6, 7, 5, 1, 15 };
@@ -77,6 +81,6 @@ public class App {
         System.out.println(
         "Resultado inserccion Metodo 3: " +
         java.util.Arrays.toString(arregloInsercionOrdenado3));
-
+        */
     }
 }
---
+++ src/BenchMarking.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class BenchMarking {
+    private MetodosOrdenamiento mO;
+    public BenchMarking(){
+
+        
+        long currentMilles = System.currentTimeMillis();
+        long currentName = System.nanoTime();
+        mO = new MetodosOrdenamiento();
+        int[] arreglo = generarArregloAleatorio(10000000);
+        Runnable tarea =()-> mO.burbujaTradicional(arreglo);
+        double tiempoDuracionMili = medirConCurrentMilles(tarea);
+        double tiempoDuracionNano = medirConCurrentTime(tarea);
+        System.out.println(currentMilles);
+        System.out.println(currentName);
+        System.out.println("Tiempo en mili segundos: "+ tiempoDuracionMili+" Tiempo duracion en nano Segundos: "+ tiempoDuracionNano);
+    }
+
+    public int[] generarArregloAleatorio(int tamanio){
+        int[] arreglo = new int[tamanio];
+        Random random = new Random();
+        for(int i =0; i< tamanio; i++){
+            arreglo[i]= random.nextInt(100000000);
+        }
+        return arreglo;
+    }
+
+    public double medirConCurrentMilles(Runnable tarea){
+        long inicio = System.currentTimeMillis();
+        tarea.run();
+        long fin = System.currentTimeMillis();
+        double tiempoSegundos = (fin-inicio)/1000.0;
+        return tiempoSegundos;
+    }
+
+    public double medirConCurrentTime(Runnable tarea){
+        long inicio = System.nanoTime();
+        tarea.run();
+        long fin = System.nanoTime();
+        double tiempoSegundos = (fin-inicio)/1_000_000_000.0;
+        return tiempoSegundos;
+    }
+}
